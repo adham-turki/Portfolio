6032ed01f59e981167928c816b02991929970853
Adham Turki <adhamturki321@gmail.com>
2024-09-27 22:13:39 +0300

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import adham from '../assets/adham1.jpg';
 const Header = () => {
     return (
         <>
@@ -35,7 +36,7 @@ const Header = () => {
                         <div className="bg-[#14044c] text-white flex items-center justify-center rounded-full h-10 w-10 ">
                             <div className="profile">
                                 <img
-                                    src="https://scontent.fjrs27-1.fna.fbcdn.net/v/t39.30808-6/453404087_2314834042185022_5378942058235007424_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=XSMz9Bh8ha0Q7kNvgHuSrO9&_nc_ht=scontent.fjrs27-1.fna&oh=00_AYB9VFN3bMVOAlwOCEqj-wgpdCWENIv8ruWsPHTYsqD6YA&oe=66F61E4A"
+                                    src={adham}
                                     alt="Profile"
                                     className="rounded-full"
                                 />
