dfea5c97b8a20d3c5ce98692bfd7e0980cc8ac0b
Adham Turk <adhamibraheem321@gmail.com>
2026-04-21 21:29:19 +0300

    s

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,30 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..'],
     },
   },
   build: {
+    sourcemap: true,
     // Remove or comment out this section if you don't want to exclude the module
     // rollupOptions: {
     //   external: ['@sanity/client'],
     // },
+  },
+  esbuild: {
+    target: 'es2015',
+    drop: ['console', 'debugger'],
   },
 });
